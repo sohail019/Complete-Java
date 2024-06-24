5bb0dd9b6004a523c9adb122bf4cbc188771c3fc
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-06-24 06:44:26 +0000

    Learned about method which not return any value and not accept any parameter

---
+++ 01 - Basics/04 - Methods/Type1MethodEx1.java
@@ -0,0 +1,13 @@
+class Type1MethodEx1{
+	public static void main(String[] args){
+		add();
+	}
+	
+	public static void add(){
+		int a = 10;
+		int b = 20;
+		int res = a + b;
+		
+		System.out.println("Addition is " + res);
+	}
+}
