f88fedd4ebd832a8f674ec199d7dde585c62a87a
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-06-24 13:34:44 +0000

    Learned about Shorthand usage for Assignment plus Arithmetic Operations

---
+++ 01 - Basics/01 - Operators/ShorthandAssignmentOperator.java
@@ -0,0 +1,17 @@
+class ShorthandAssignmentOperator{
+	public static void main(String[] args){
+		int a = 10;
+		
+		a+=2; // ----> a = a + 2;
+		System.out.println(a);
+		
+		a-=2; // ----> a = a - 2;
+		System.out.println(a);
+		
+		a*=2; // ----> a = a * 2;
+		System.out.println(a);
+		
+		a/=2; // ----> a = a / 2;
+		System.out.println(a);
+	}
+}
