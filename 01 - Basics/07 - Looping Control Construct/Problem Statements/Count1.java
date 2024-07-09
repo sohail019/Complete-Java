5ba8e788570e634a2154ca6f45538d2c11d17fd6
Sohail Shaikh <69633245+sohail019@users.noreply.github.com>
2024-07-09 13:28:43 +0000

    counting numbers with nested loops

---
+++ 01 - Basics/07 - Looping Control Construct/Problem Statements/Count1.java
@@ -0,0 +1,11 @@
+public class Count1 {
+    public static void main(String[] args) {
+        int  count = 0;
+
+        for(int i = 1; i <= 10; i++){
+            count++;
+        }
+
+        System.out.println(count);
+    }    
+}
---
+++ 01 - Basics/07 - Looping Control Construct/Problem Statements/Count3.java
@@ -0,0 +1,15 @@
+
+public class Count3 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        for(int i = 1; i <= 5; i++){
+
+            for(int j = 1; j <= 5; j++){
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }  
+}  
